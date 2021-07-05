7a5711dcecc66a36572ae29b889d2ffef78974ca
Yash Karnik <yashkarnik2000@gmail.com>
2021-07-06 00:45:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const BASE_API_URI =
 	process.env.NODE_ENV == 'development'
 		? 'http://localhost:3001'
-		: 'https://banking-system-ne4mn7884-yashkarnik.vercel.app';
+		: 'https://banking-system-pi.vercel.app/';
 module.exports = {
 	env: {
 		BASE_API_URI: BASE_API_URI,
