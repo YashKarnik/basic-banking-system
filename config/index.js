674373386e3fabf7df2b1f216a4a046e9fd186d6
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-07-06 18:35:31 +0530

    Add workflow for static

--- config/index.js
+++ config/index.js
@@ -1,26 +1,14 @@
-// export const server = dev
-// 	? 'http://localhost:3001'
-// 	: 'https://banking-system-pi.vercel.app';
-
-// let server;
-// if (process.env.NODE_ENV == 'git-production')
-// 	server = 'https://yashkarnik.github.io/basic-banking-system/';
-// else {
-// 	if (process.env.NODE_ENV != 'production') server = 'http://localhost:3001';
-// 	else server = 'https://banking-system-pi.vercel.app';
-// }
-// console.log(server);
-// export { server };
 const expression = process.env.NODE_ENV;
 let server;
 switch (expression) {
-	case 'git-production':
+	case 'production':
 		server = 'https://yashkarnik.github.io/basic-banking-system';
 		break;
-	case 'production':
-		server = 'https://banking-system-pi.vercel.app';
+	case 'development':
+		server = 'http://localhost:3001';
 		break;
 	default:
 		server = 'http://localhost:3001';
+		break;
 }
 export { server };
