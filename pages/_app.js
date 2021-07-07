f83e8e6e0ea805f53b8d81a65a949e45ab756479
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-07-07 20:57:19 +0530

    use page transition loaidng

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Loading from '../components/loading';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	const [loading, setLoading] = useState(false);
+	const router = useRouter();
+	useEffect(() => {
+		const handleStart = url => setLoading(true);
+		const handleComplete = url => setLoading(false);
+
+		router.events.on('routeChangeStart', handleStart);
+		router.events.on('routeChangeComplete', handleComplete);
+		router.events.on('routeChangeError', handleComplete);
+
+		return () => {
+			router.events.off('routeChangeStart', handleStart);
+			router.events.off('routeChangeComplete', handleComplete);
+			router.events.off('routeChangeError', handleComplete);
+		};
+	}, []);
+
+	if (loading) return <Loading isFull={true} />;
+	else return <Component {...pageProps} />;
 }
 
 export default MyApp;
